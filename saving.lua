a1d27eefbf9ddc0ca0a38419e0eea951b7603f1e
Luna Nova <git@lunnova.dev>
2024-09-29 14:20:49 -0700

    wip

--- saving.lua
+++ saving.lua
@@ -2,6 +2,18 @@ local terms = require "terms"
 local gen = require "terms-generators"
 local derivers = require "derivers"
 
+-- the state is a combination of what needs to actually be saved
+-- and lookup tables
+-- after finsih loading stuff in to serialization state
+-- create object storing just stuff to store
+-- can toss the whole thing to json
+
+-- api is calling serialize(term),
+-- starts by creating a new serialization state,
+-- serializes into the state
+-- returns state
+-- deserialize(state) returns a term that's __eq to the original
+
 ---@class SerializedEnum
 ---@field kind string
 ---@field args integer[]
@@ -22,31 +34,118 @@ local derivers = require "derivers"
 ---@type {[Type] : fun(SerializationState, any) : SerializedID}
 local serializers = {}
 ---@type {[Type] : fun(SerializationState, SerializedID) : unknown}
-local serializers = {}
 local deserializers = {}
+
+-- Serializers for Lua built-in types
+
+serializers["number"] = function(state, subject)
+	return subject -- Numbers can be serialized as-is
+end
+
+serializers["string"] = function(state, subject)
+	return subject -- Strings can be serialized as-is
+end
+
+serializers["boolean"] = function(state, subject)
+	return subject -- Booleans can be serialized as-is
+end
+
+serializers["nil"] = function(state, subject)
+	return subject -- nil can be serialized as-is
+end
+
+-- Deserializers for Lua built-in types
+
+deserializers["number"] = function(state, id)
+	return id -- Numbers can be deserialized as-is
+end
+
+deserializers["string"] = function(state, id)
+	return id -- Strings can be deserialized as-is
+end
+
+deserializers["boolean"] = function(state, id)
+	return id -- Booleans can be deserialized as-is
+end
+
+deserializers["nil"] = function(state, id)
+	return id -- nil can be deserialized as-is
+end
+
+-- Handle tables (non-metatabled)
+serializers["table"] = function(state, subject)
+	local serialized = {}
+	for k, v in pairs(subject) do
+		if k == nil then
+			error("Cannot serialize table with nil key")
+		end
+		if v == nil then
+			error("Cannot serialize table with nil value")
+		end
+		serialized[serialize(state, k)] = serialize(state, v)
+	end
+	return serialized
+end
+
+deserializers["table"] = function(state, id)
+	local deserialized = {}
+	for k, v in pairs(id) do
+		deserialized[deserialize(state, k)] = deserialize(state, v)
+	end
+	return deserialized
+end
 
 ---serialize a value of unknown type
 ---@param state SerializationState
 ---@param subject any
 ---@return SerializedID
-local function serialize(state, subject) end
+local function serialize(state, subject)
+	if subject == nil then
+		error("Cannot serialize nil value")
+	end
+	local stype = type(subject)
+	if stype == "table" then
+		stype = getmetatable(subject) or "table"
+	end
+	if not serializers[stype] then
+		error("No serializer implemented for type: " .. tostring(stype))
+	end
+	return serializers[stype](state, subject)
+end
 
 ---deserialize a value of unknown type
 ---@param state SerializationState
 ---@param id SerializedID
 ---@return any
-local function deserialize(state, id) end
+local function deserialize(state, id)
+	local stype = type(id)
+	if stype == "table" then
+		if not id.kind then
+			error("Invalid serialization ID: missing 'kind' field")
+		end
+		stype = id.kind
+	end
+	if not deserializers[stype] then
+		error("No deserializer implemented for type: " .. tostring(stype))
+	end
+	return deserializers[stype](state, id)
+end
+
 ---serialize a value of a known type
 ---@param state SerializationState
 ---@param stype Type
 ---@param subject any
 ---@return SerializedID
 local function serialize_known(state, stype, subject)
-	if not serializers[stype] then
-		error "known type has no serializer implemented"
+	if subject == nil then
+		error("Cannot serialize nil value")
 	end
-	return serializers[type](state, subject)
+	if not serializers[stype] then
+		error("Known type has no serializer implemented: " .. tostring(stype))
+	end
+	return serializers[stype](state, subject)
 end
+
 ---deserialize a value of a known type
 ---@param state SerializationState
 ---@param stype Type
@@ -54,17 +153,15 @@ end
 ---@return any
 local function deserialize_known(state, stype, id)
 	if not deserializers[stype] then
-		error "known type has no deserializer implemented"
+		error("Known type has no deserializer implemented: " .. tostring(stype))
 	end
-	return deserializers[type](state, id)
+	return deserializers[stype](state, id)
 end
 
 ---@type Deriver
--- TODO: boilerplate done, implement actual serialize
 local serialize_deriver = {
 	record = function(t, info)
 		if t.derived_serialize then
-			-- already derived, don't derive again
 			return
 		end
 
@@ -73,16 +170,36 @@ local serialize_deriver = {
 		local kind = info.kind
 		local params = info.params
 
-		local function serialize_fn(self)
-			-- this function runs when you do myrecordterm:serialize()
+		serializers[t] = function(state, self)
+			local serialized = { kind = kind, args = {} }
+			for _, param in ipairs(params) do
+				if self[param] == nil then
+					error("Missing parameter '" .. param .. "' in record of type '" .. kind .. "'")
+				end
+				table.insert(serialized.args, serialize(state, self[param]))
+			end
+			table.insert(state.construction, serialized)
+			return #state.construction
 		end
-		idx.serialize = serialize_fn
+
+		deserializers[t] = function(state, id)
+			local serialized = state.construction[id]
+			local deserialized = {}
+			for i, param in ipairs(params) do
+				deserialized[param] = deserialize(state, serialized.args[i])
+			end
+			return setmetatable(deserialized, t)
+		end
+
+		idx.serialize = function(self)
+			local state = { construction = {}, lookup = {} }
+			return serialize(state, self), state
+		end
 
 		t.derived_serialize = true
 	end,
 	enum = function(t, info)
 		if t.derived_serialize then
-			-- already derived, don't derive again
 			return
 		end
 
@@ -91,24 +208,57 @@ local serialize_deriver = {
 		local name = info.name
 		local variants = info.variants
 
-		for _, vname in ipairs(variants) do
-			local vkind = name .. "." .. vname
+		serializers[t] = function(state, self)
+			local vname = self.kind:sub(#name + 2)
 			local vdata = variants[vname]
-			local vtype = vdata.type
-			local vinfo = vdata.info
-			if vtype == derivers.EnumDeriveInfoVariantKind.Record then
-				for i, param in ipairs(vinfo.params) do
-				end
-			elseif vtype == derivers.EnumDeriveInfoVariantKind.Unit then
-			else
-				error("unknown variant type: " .. vtype)
+			if not vdata then
+				error("Unknown variant '" .. vname .. "' for enum '" .. name .. "'")
 			end
+			local serialized = { kind = self.kind, args = {} }
+
+			if vdata.type == derivers.EnumDeriveInfoVariantKind.Record then
+				for _, param in ipairs(vdata.info.params) do
+					if self[param] == nil then
+						error(
+							"Missing parameter '"
+								.. param
+								.. "' in enum variant '"
+								.. vname
+								.. "' of type '"
+								.. name
+								.. "'"
+						)
+					end
+					table.insert(serialized.args, serialize(state, self[param]))
+				end
+			end
+
+			table.insert(state.construction, serialized)
+			return #state.construction
 		end
 
-		local function serialize_fn(self)
-			-- this function runs when you do myenumterm:serialize()
+		deserializers[t] = function(state, id)
+			local serialized = state.construction[id]
+			local vname = serialized.kind:sub(#name + 2)
+			local vdata = variants[vname]
+			if not vdata then
+				error("Unknown variant '" .. vname .. "' for enum '" .. name .. "'")
+			end
+			local deserialized = { kind = serialized.kind }
+
+			if vdata.type == derivers.EnumDeriveInfoVariantKind.Record then
+				for i, param in ipairs(vdata.info.params) do
+					deserialized[param] = deserialize(state, serialized.args[i])
+				end
+			end
+
+			return setmetatable(deserialized, t)
 		end
-		idx.serialize = serialize_fn
+
+		idx.serialize = function(self)
+			local state = { construction = {}, lookup = {} }
+			return serialize(state, self), state
+		end
 
 		t.derived_serialize = true
 	end,
@@ -117,41 +267,124 @@ local serialize_deriver = {
 	end,
 	map = function(t, info)
 		if t.derived_serialize then
-			-- already derived, don't derive again
 			return
 		end
 
 		local idx = t.__index
 
-		local function serialize_fn(self) end
-		idx.serialize = serialize_fn
+		serializers[t] = function(state, self)
+			local serialized = { kind = "map", args = {} }
+			for k, v in pairs(self._map) do
+				if k == nil then
+					error("Cannot serialize map with nil key")
+				end
+				if v == nil then
+					error("Cannot serialize map with nil value")
+				end
+				table.insert(serialized.args, serialize(state, k))
+				table.insert(serialized.args, serialize(state, v))
+			end
+			table.insert(state.construction, serialized)
+			return #state.construction
+		end
+
+		deserializers[t] = function(state, id)
+			local serialized = state.construction[id]
+			local deserialized = t()
+			for i = 1, #serialized.args, 2 do
+				local k = deserialize(state, serialized.args[i])
+				local v = deserialize(state, serialized.args[i + 1])
+				deserialized:set(k, v)
+			end
+			return deserialized
+		end
+
+		idx.serialize = function(self)
+			local state = { construction = {}, lookup = {} }
+			return serialize(state, self), state
+		end
 
 		t.derived_serialize = true
 	end,
 	set = function(t, info)
 		if t.derived_serialize then
-			-- already derived, don't derive again
 			return
 		end
 
 		local idx = t.__index
 
-		local function serialize_fn(self) end
-		idx.serialize = serialize_fn
+		serializers[t] = function(state, self)
+			local serialized = { kind = "set", args = {} }
+			for k in pairs(self._set) do
+				if k == nil then
+					error("Cannot serialize set with nil value")
+				end
+				table.insert(serialized.args, serialize(state, k))
+			end
+			table.insert(state.construction, serialized)
+			return #state.construction
+		end
+
+		deserializers[t] = function(state, id)
+			local serialized = state.construction[id]
+			local deserialized = t()
+			for _, arg in ipairs(serialized.args) do
+				deserialized:put(deserialize(state, arg))
+			end
+			return deserialized
+		end
+
+		idx.serialize = function(self)
+			local state = { construction = {}, lookup = {} }
+			return serialize(state, self), state
+		end
 
 		t.derived_serialize = true
 	end,
 	array = function(t, info)
 		if t.derived_serialize then
-			-- already derived, don't derive again
 			return
 		end
 
 		local methods = t.methods
 
-		local function serialize_fn(self) end
-		methods.serialize = serialize_fn
+		serializers[t] = function(state, self)
+			local serialized = { kind = "array", args = {} }
+			for i = 1, self:len() do
+				if self[i] == nil then
+					error("Cannot serialize array with nil value at index " .. i)
+				end
+				table.insert(serialized.args, serialize(state, self[i]))
+			end
+			table.insert(state.construction, serialized)
+			return #state.construction
+		end
+
+		deserializers[t] = function(state, id)
+			local serialized = state.construction[id]
+			local deserialized = t()
+			for _, arg in ipairs(serialized.args) do
+				deserialized:push(deserialize(state, arg))
+			end
+			return deserialized
+		end
+
+		methods.serialize = function(self)
+			local state = { construction = {}, lookup = {} }
+			return serialize(state, self), state
+		end
 
 		t.derived_serialize = true
 	end,
+}
+
+-- Export the necessary functions and tables
+return {
+	serialize = serialize,
+	deserialize = deserialize,
+	serialize_known = serialize_known,
+	deserialize_known = deserialize_known,
+	serializers = serializers,
+	deserializers = deserializers,
+	serialize_deriver = serialize_deriver,
 }
---
+++ test-saving.lua
@@ -0,0 +1,82 @@
+local terms = require "terms"
+local saving = require "saving"
+local gen = require "terms-generators"
+local array = gen.declare_array
+local value_array = array(terms.value)
+
+-- Create some sample terms
+local sample_number = terms.value.number(42)
+local sample_string = terms.value.host_value("Hello, world!")
+local sample_tuple =
+	terms.value.tuple_value(value_array(terms.value.number(1), terms.value.host_value("two"), terms.value.number(3)))
+
+local sample_enum = terms.value.enum_value(
+	"cons",
+	terms.tup_val(terms.value.number(1), terms.value.enum_value("empty", terms.tup_val()))
+)
+
+terms.value:derive(saving.serialize_deriver)
+terms.neutral_value:derive(saving.serialize_deriver)
+terms.typed_term:derive(saving.serialize_deriver)
+terms.inferrable_term:derive(saving.serialize_deriver)
+terms.checkable_term:derive(saving.serialize_deriver)
+terms.binding:derive(saving.serialize_deriver)
+terms.expression_goal:derive(saving.serialize_deriver)
+terms.free:derive(saving.serialize_deriver)
+terms.visibility:derive(saving.serialize_deriver)
+terms.purity:derive(saving.serialize_deriver)
+terms.result_info:derive(saving.serialize_deriver)
+
+-- Test the serialization for each term type
+local function test_serialization(term_name, term)
+	local ok, result = pcall(function()
+		local state = saving.serialize(term)
+		local loaded = saving.deserialize(state)
+		if not terms.value.__eq(term, loaded) then
+			p("Term:", term, "Type:", type(term))
+			p("Loaded:", loaded, "Type:", type(loaded))
+			if type(term) == "table" and type(loaded) == "table" and term.default_print and loaded.default_print then
+				print("Term (default_print):")
+				print(term:default_print())
+				print("Loaded (default_print):")
+				print(loaded:default_print())
+			end
+			error(term_name .. " saving/loading mismatch")
+		end
+		return true
+	end)
+	if ok then
+		print(term_name .. " saving/loading passed")
+	else
+		print(term_name .. " saving/loading failed")
+		print("Error: " .. tostring(result))
+		print("Attempted to serialize:")
+		p(term)
+	end
+end
+
+test_serialization("Number", sample_number)
+test_serialization("String", sample_string)
+test_serialization("Tuple", sample_tuple)
+test_serialization("Enum", sample_enum)
+
+-- Additional tests using test_serialization function
+local sample_level = terms.value.level(3)
+test_serialization("Level", sample_level)
+
+local sample_star = terms.value.star(2, 1)
+test_serialization("Star", sample_star)
+
+local sample_prop = terms.value.prop(1)
+test_serialization("Prop", sample_prop)
+
+local sample_pi = terms.value.pi(
+	terms.value.number_type,
+	terms.value.param_info(terms.value.visibility(terms.visibility.explicit)),
+	terms.value.number_type,
+	terms.value.result_info(terms.result_info(terms.purity.pure))
+)
+test_serialization("Pi", sample_pi)
+
+local sample_closure = terms.value.closure("x", terms.typed_term.literal(terms.unit_val), terms.runtime_context())
+test_serialization("Closure", sample_closure)
